22bfefcb6470d0c7006d16192f39d27b8c954d32
Johannes <j.hinterstoesser@live.de>
2020-07-11 09:29:54 +0200

    build: Added wrtie to disk for webpack dev server

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "production",
-  devtool: 'source-map',
+  devtool: "source-map",
   module: {
     rules: [
       {
@@ -25,6 +25,6 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
-    new WorkboxPlugin.GenerateSW()
+    new WorkboxPlugin.GenerateSW(),
   ],
 };
